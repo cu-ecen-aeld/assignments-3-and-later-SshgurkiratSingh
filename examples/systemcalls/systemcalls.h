ad1ecef5cc991ef4022b96ab45c541271fb2e1ff
Gurkirat Singh <63462904+SshgurkiratSingh@users.noreply.github.com>
2024-06-25 21:26:02 +0530

    Automated Push

--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdarg.h>
+#include <stdarg.h> // va_list part of it handles variable length arguments
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
 
 bool do_system(const char *command);
 
